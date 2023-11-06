fb9c8f95e3f69938f553fae96737b6c3ed526fa9
Janith <janithscrace@gmail.com>
2023-11-06 19:12:21 +0530

    Update Cystal Report

--- Resturent Management System/Resturent Management System/Properties/Resources.Designer.cs
+++ Resturent Management System/Resturent Management System/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace Resturent_Management_System.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap print {
+            get {
+                object obj = ResourceManager.GetObject("print", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap product {
             get {
                 object obj = ResourceManager.GetObject("product", resourceCulture);
